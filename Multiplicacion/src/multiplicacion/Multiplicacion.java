418fa4fd6bd643a6d89b0904ecc1a135fc879e01
giangrobert <gianmamani93@gmail.com>
2020-04-26 12:33:16 -0500

    Desarrollo de la multiplicación

---
+++ Multiplicacion/src/multiplicacion/Multiplicacion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiplicacion;
+
+/**
+ *
+ * @author gianm
+ */
+public class Multiplicacion {
+    
+    public static void main(String[] args) {
+        multiplicacion.index ol=new multiplicacion.index();
+        ol.show();
+       
+    }
+    
+}
